186a98f1df60309ae4dcc4338bf6c0c15a025de3
Barış Soner Uşaklı <barisusakli@gmail.com>
2023-03-18 12:51:36 -0400

    update for 2.x

--- public/admin.js
+++ public/admin.js
@@ -1,15 +1,14 @@
-/* global $, app, define */
-/* eslint no-var: 0, prefer-arrow-callback: 0, func-names: 0, strict: 0 */
+define('admin/plugins/cdn', ['alerts'], function (alerts) {
+	$('#save').click(function () {
+		const settings = {
+			url: $('#url').val(),
+			enabled: $('#enabled').prop('checked'),
+		};
 
-define('admin/plugins/cdn', [], function () {
-  $('#save').click(function () {
-    var settings = {
-      url: $('#url').val(),
-      enabled: $('#enabled').prop('checked'),
-    };
-
-    $.get('/api/admin/plugins/cdn/save', { settings: JSON.stringify(settings) }, function () {
-      app.alertSuccess();
-    });
-  });
+		$.get('/api/admin/plugins/cdn/save', {
+			settings: JSON.stringify(settings),
+		}, function () {
+			alerts.success();
+		});
+	});
 });
